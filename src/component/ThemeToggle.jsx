c552d58fc42070b94174d7c858d31f084dfaebc5
MicheleFamoso <michelefamoso@gmail.com>
2025-10-13 10:50:07 +0200

    Refactor ThemeToggle to use custom buttons
    Replaced the Button component with custom-styled button elements for theme selection. Improved UI by providing separate buttons for light and dark modes, enhancing clarity and accessibility.

--- src/component/ThemeToggle.jsx
+++ src/component/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Button from "./atoms/Button";
-import { MoonIcon,SunIcon } from "@heroicons/react/24/outline";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 const ThemeToggle = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
@@ -17,10 +17,26 @@ const ThemeToggle = () => {
 
   return (
     <>
-      <Button
+      {/* <Button
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         text={theme === "light" ? <SunIcon className="size-7"/> : <MoonIcon className="size-7"/>}
-      ></Button>
+      ></Button> */}
+
+      <div className="bg-bg-light flex gap-6  rounded-3xl py-1 dark:bg-bg-dark w-fit">
+       
+        <button
+        className={`dark:text-text-secondary-dark  text-text-secondary-light py-1 px-6 rounded-4xl  cursor-pointer hover:bg-bg-list-light dark:hover:bg-icon-gray-dark   ${theme === "light"? " bg-btn-light  dark:bg-btn-dark shadow-xl ":""} `}
+        onClick={()=>setTheme("light")}
+        >
+          <SunIcon className="size-7 " />
+        </button>
+        <button
+         className={` dark:text-text-secondary-dark  text-text-secondary-light py-1 px-6 rounded-4xl  cursor-pointer hover:bg-btn-light dark:hover:bg-icon-gray-dark   ${theme === "dark"? " bg-btn-light dark:bg-btn-dark " : ""} `}
+     onClick={()=>setTheme("dark")}
+        >
+          <MoonIcon className="size-7" />
+        </button>
+      </div>
     </>
   );
 };
