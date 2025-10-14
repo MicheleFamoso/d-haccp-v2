4010d55e276ee1e26ba372584cc0b19cd8313637
MicheleFamoso <michelefamoso@gmail.com>
2025-10-14 08:43:33 +0200

    Refactor Button component styles and formatting
    Updated Button component to improve code formatting and consistency. Changed font weight from medium to bold, added text-shadow to accent variant, and ensured export statement ends with a semicolon.

--- src/component/atoms/Button.jsx
+++ src/component/atoms/Button.jsx
@@ -1,21 +1,19 @@
+const VARIANTS = {
+  primary: " dark:text-text-secondary-dark  text-text-secondary-light",
+  secondary: " dark:text-accent-red  text-accent-red",
+  accent: "dark:text-accent-blue-dark text-accent-blue-medium text-shadow-xl ",
+};
 
+const Button = ({ text, variant = "primary", onClick }) => {
+  const variantClasses = VARIANTS[variant] || VARIANTS.primary;
+  return (
+    <button
+      onClick={onClick}
+      className={`${variantClasses} hover:bg-btn-light bg-bg-list-light font-h font-bold dark:bg-btn-dark  py-1 px-3 rounded-4xl  cursor-pointer  dark:hover:bg-icon-gray-dark dark:shadow-xs shadow-xl`}
+    >
+      {text}
+    </button>
+  );
+};
 
-const VARIANTS ={
-    primary:" dark:text-text-secondary-dark  text-text-secondary-light",
-    secondary:" dark:text-accent-red  text-accent-red",
-    accent:"dark:text-accent-blue-dark text-accent-blue-medium"
-
-}
-
-const Button =({text,variant = "primary", onClick})=>{
-    const variantClasses = VARIANTS[variant] || VARIANTS.primary
-    return(
-        <button
-        onClick={onClick}
-        className={ `${variantClasses} hover:bg-btn-light bg-bg-list-light font-h font-medium dark:bg-btn-dark  py-1 px-3 rounded-4xl  cursor-pointer  dark:hover:bg-icon-gray-dark dark:shadow-xs shadow-xl`}
-        >{text}</button>
-    )
-
-}
-
-export default Button
+export default Button;
