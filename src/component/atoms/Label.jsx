6f78f1741bfdab03f8de21b561ac447a31b3b7b8
MicheleFamoso <michelefamoso@gmail.com>
2025-10-14 12:19:35 +0200

    Refactor Label component formatting
    Improved code readability and formatting in the Label component by adjusting indentation and adding consistent spacing. No functional changes were made.

--- src/component/atoms/Label.jsx
+++ src/component/atoms/Label.jsx
@@ -1,13 +1,11 @@
-const Label=({htmlFor,text})=>{
-return (
-
-     <label htmlFor={htmlFor}
-            className="dark:text-text-secondary-dark text-text-secondary-light font-h text-md  ml-4"
-            >
-                {text}
-            </label>
-
-             
-)
-}
-export default Label
+const Label = ({ htmlFor, text }) => {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className="dark:text-text-secondary-dark text-text-secondary-light font-h text-md  ml-4"
+    >
+      {text}
+    </label>
+  );
+};
+export default Label;
