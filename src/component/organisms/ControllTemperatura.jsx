30e320ca5caf49a449c0564590590ec76f41fc16
MicheleFamoso <michelefamoso@gmail.com>
2025-10-20 15:45:45 +0200

    Update ControllTemperatura.jsx

--- src/component/organisms/ControllTemperatura.jsx
+++ src/component/organisms/ControllTemperatura.jsx
@@ -1,25 +1,48 @@
 import { useState, useEffect } from "react";
+import { data } from "react-router-dom";
 
 const ControllTemperatura = () => {
+  //const Fetch
   const [temperature, SetTemperature] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  //Const data odierna
   const [dataControllo, setdataControllo] = useState(
     new Date().toISOString().split("T")[0]
   );
 
+  //Giorno odierno
+  const x = new Date(dataControllo);
+  const oggi = x.getDate();
+  const mese = x.toLocaleString("it-IT", { month: "short" });
+
+  // Funzione giorno precedente
   const giornoPrecedente = () => {
     const nuovaData = new Date(dataControllo);
     nuovaData.setDate(nuovaData.getDate() - 1);
     setdataControllo(nuovaData.toISOString().split("T")[0]);
   };
 
+  //Const per render giorno prec
+  const d = new Date(dataControllo);
+  d.setDate(d.getDate() - 1);
+  const giornoPrec = d.getDate();
+  const mesePrec = d.toLocaleString("it-IT", { month: "short" });
+
+  //Funzione giorno succ
   const giornoSuccessivo = () => {
     const nuovaData = new Date(dataControllo);
     nuovaData.setDate(nuovaData.getDate() + 1);
     setdataControllo(nuovaData.toISOString().split("T")[0]);
   };
+  //Const per render giorno succ
+  const e = new Date(dataControllo);
+  e.setDate(e.getDate() + 1);
+  const giornoSucc = d.getDate();
+  const meseSucc = d.toLocaleString("it-IT", { month: "short" });
 
+  //Fetch get
   const HandleTemp = async () => {
     const token = localStorage.getItem("token");
     try {
@@ -43,6 +66,8 @@ const ControllTemperatura = () => {
       setError(error.message);
     }
   };
+
+  //UseEffect con dataControllo cosi da ri-renderizzare ad ogni cambio di data
   useEffect(() => {
     HandleTemp();
   }, [dataControllo]);
@@ -51,7 +76,10 @@ const ControllTemperatura = () => {
     <div className="h-10/12">
       <nav>
         {" "}
-        <button onClick={() => giornoPrecedente()}>{dataControllo}</button>
+        <button onClick={() => giornoPrecedente()} className="bg-red-50 p-4">
+          {giornoPrec} {mesePrec}
+        </button>
+        <p>{oggi}</p>
       </nav>
 
       <h1>Termperatura</h1>
