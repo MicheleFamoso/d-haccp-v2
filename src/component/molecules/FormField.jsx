bc2a4713a89e88fec0dc7435de65f51c74db3339
MicheleFamoso <michelefamoso@gmail.com>
2025-11-03 18:25:51 +0100

    fix

--- src/component/molecules/FormField.jsx
+++ src/component/molecules/FormField.jsx
@@ -1,21 +1,13 @@
-import Input from "../atoms/Input"
-import Label from "../atoms/Label"
+import Input from "../atoms/Input";
+import Label from "../atoms/Label";
 
-const FormField = ({id,value,onChange,text, type = "text"})=>{
-    return(
-        <div className="flex flex-col gap-1 mb-4">
-            <Label
-            htmlFor={id}
-               text={text} 
-            />
-            <Input
-             value={value}
-               onChange={onChange}
-                 id={id}
-                 type = {type}
-            />
-        </div>
-    )
-}
+const FormField = ({ id, value, onChange, text, type = "text" }) => {
+  return (
+    <div className="flex flex-col gap-1 mb-4">
+      <Label htmlFor={id} text={text} />
+      <Input value={value} onChange={onChange} id={id} type={type} />
+    </div>
+  );
+};
 
-export default FormField 
+export default FormField;
