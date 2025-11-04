0da1d326dd88a98a4e2b8218383076d423cf614d
MicheleFamoso <michelefamoso@gmail.com>
2025-11-04 15:48:11 +0100

    Update CardInfestanti.jsx

--- src/component/organisms/CardInfestanti.jsx
+++ src/component/organisms/CardInfestanti.jsx
@@ -1,12 +1,56 @@
-import { Check, TriangleAlert, Calendar, Rat, Bird, Bug } from "lucide-react";
+import {
+  Check,
+  TriangleAlert,
+  Calendar,
+  Rat,
+  Bird,
+  Bug,
+  Ellipsis,
+  Trash,
+} from "lucide-react";
+import { useState, useRef, useEffect } from "react";
 
-const CardInfestanti = ({ infestante }) => {
+const CardInfestanti = ({ infestante, onDelete }) => {
+  const [menu, setMenu] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
   return (
     <div
       key={infestante.id}
       className="font-h  bg-bg-light/50 border-2 border-white  dark:bg-btn-dark dark:border-bg-list-dark   md:px-5 md:pt-2 shadow-md px-4 pt-0 pb-4 rounded-3xl font-bold select-none text-text-secondary-light dark:text-text-primary-dark"
     >
-      {" "}
+      <div ref={menuRef} className="flex justify-end relative">
+        <button
+          onClick={() => setMenu(!menu)}
+          className="hover:bg-text-tertiary-light p-1 rounded-full cursor-pointer"
+        >
+          <Ellipsis className="hover:text-white" />
+        </button>
+        {menu && (
+          <div className="absolute right-0 top-8 bg-white/60 dark:bg-bg-list-dark/90 border-1 backdrop-blur-sm border-white dark:border-bg-list-dark   rounded-2xl shadow-2xl">
+            <div
+              onClick={() => {
+                onDelete(infestante.id);
+                setMenu(false);
+              }}
+              className="flex items-center hover:bg-alert-2 px-6  hover:text-white py-2 rounded-3xl gap-3"
+            >
+              <Trash size={22} className=" cursor-pointer" />
+              <button className=" cursor-pointer">Elimina</button>
+            </div>
+          </div>
+        )}
+      </div>{" "}
       <div className="flex items-center justify-between mb-4">
         <div className="flex gap-2 items-center text-text-primary-light font-medium dark:text-text-primary-dark">
           <Calendar
