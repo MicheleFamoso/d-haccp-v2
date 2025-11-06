036ba7edeab31748571c82f33ba9ff8af88a3139
MicheleFamoso <michelefamoso@gmail.com>
2025-11-06 16:21:29 +0100

    Add AddDipendenti Component

---
+++ src/component/organisms/addComponent/AddDipendenti.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import FormField from "../../molecules/FormField";
+import Button from "../../atoms/Button";
+
+const AddDipendenti = () => {
+  const [dipendete, SetDipendente] = useState({
+    nome: "",
+    cognome: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleDipendenti = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("http://localhost:8080/admin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(dipendete),
+      });
+      if (!response.ok)
+        throw new Error("Errore neel'aggiuunta di un dipendente ");
+      SetDipendente({
+        nome: "",
+        cognome: "",
+        username: "",
+        email: "",
+        password: "",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="font-h text-xl font-bold text-text-secondary-light dark:text-text-primary-dark text-center text-shadow-xs mb-4">
+        Aggiungi Dipendente
+      </h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleDipendenti();
+        }}
+      >
+        <FormField
+          text={"Nome"}
+          value={dipendete.nome}
+          onChange={(e) =>
+            SetDipendente({ ...dipendete, nome: e.target.value })
+          }
+        />
+        <FormField
+          text={"Cognome"}
+          value={dipendete.cognome}
+          onChange={(e) =>
+            SetDipendente({ ...dipendete, cognome: e.target.value })
+          }
+        />
+        <FormField
+          text={"Username"}
+          value={dipendete.username}
+          onChange={(e) =>
+            SetDipendente({ ...dipendete, username: e.target.value })
+          }
+        />
+        <FormField
+          text={"Email"}
+          type={"email"}
+          value={dipendete.email}
+          onChange={(e) =>
+            SetDipendente({ ...dipendete, email: e.target.value })
+          }
+        />
+        <FormField
+          text={"Password"}
+          type={"password"}
+          value={dipendete.password}
+          onChange={(e) =>
+            SetDipendente({ ...dipendete, password: e.target.value })
+          }
+        />
+        <Button type={"submit"} text={"aggiungi"} className="w-full mt-4" />
+      </form>
+    </div>
+  );
+};
+export default AddDipendenti;
--- src/component/pages/Dipendenti.jsx
+++ src/component/pages/Dipendenti.jsx
@@ -1,9 +1,13 @@
+import AddDipendenti from "../organisms/addComponent/AddDipendenti";
+
 const Dipendenti = () => {
   return (
     <div>
       <div className="grid grid-col-1 xl:grid-cols-3 3xl:grid-cols-4  gap-4 xl:items-start">
         <div className="bg-section-light xl:col-span-2 3xl:col-span-3  p-5 rounded-3xl shadow-lg dark:bg-section-dark 3xl:h-[800px] h-[750px] overflow-y-auto no-scrollbar "></div>
-        <div className="bg-section-light px-3  py-5 rounded-3xl shadow-lg dark:bg-section-dark"></div>
+        <div className="bg-section-light px-3  py-5 rounded-3xl shadow-lg dark:bg-section-dark">
+          <AddDipendenti />
+        </div>
       </div>
     </div>
   );
